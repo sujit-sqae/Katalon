055a8751b6ed49b3dd9f01586317026fe92e8849
Sarker <sujit.sqae@gmail.com>
2023-09-02 00:49:57 +0600

    Full project

---
+++ Support/README.md
@@ -0,0 +1,20 @@
+Scenario 2: Mobile Application Automation
+
+To perform this automation testing, I used Katalon Studio as a testing tool.
+
+#Project Setup
+1. Install Appium [I prefer Appium version 1.22.0 because I found it to be most compatible with any OS version of Mac or Windows].
+2. Install Katalon Studio
+3. Install the given apk on your mobile device
+4. Mobile device deleoper options should be ON, connected through a cable, and USB debugging should be ON [device settings need to be changed for different OS, models, and brands]
+5. Open the "Source Code" folder in Katalon Studio
+6. Expand "Test Suits and click on "Calculator"
+7. Now go to "Action" > "Run" > "Android"
+8. From the pop-up, select your device and click on "OK"
+
+
+Other Features:
+1. A test report will be automatically generated inside the Reports folder
+
+
+
---
+++ Test Suites/Calculator.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Calculator</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0ac6ae28-5070-40cd-ba04-d85cf0e677ac</testSuiteGuid>
+   <testCaseLink>
+      <guid>4faff255-41be-414f-bb64-030833f8fad4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Addition</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fe78f74c-3b8a-4c08-b19f-9ffe610fc315</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Multiplication</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>03acd2de-58cb-4ac8-9206-fd7b39618ba2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Division</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7239d50f-a154-49e8-a77a-294058784137</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Subtraction</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>55440edb-5f6f-493d-a137-f2af48fdcdca</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/All Mathematical Operations</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
